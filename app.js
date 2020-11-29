adf8e4cd5b85fd6d6ffcc1115c6601533a0d60ce
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-11-29 09:08:51 -0300

    deploy app feito no desafio 21 dias

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var path = require('path');
-var secure = require('express-force-https');
+// var secure = require('express-force-https');
 const RateLimit = require('express-rate-limit');
  
 var app = express();
 
-app.use(secure);
+// app.use(secure);
 
 app.use(express.static(path.join(__dirname, 'public')));
  
